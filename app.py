79bae1959d647891fe2f27482fa991555ed9f182
Sreeya beesupaka <103497608+SreeyaB@users.noreply.github.com>
2023-11-16 22:30:32 +0530

    Add files via upload
    Mini project 
    Title:- Hand gesture recognition using opencv and tensorflow

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import subprocess
+import signal
+import os
+
+def main():
+    st.title("Hand Gesture Recognition using OpenCV, TensorFlow, and MediaPipe")
+
+    # Add a selector for the app mode on the sidebar
+    app_mode = st.sidebar.selectbox("Choose the app mode",["","About the project", "Run the project"])
+    if app_mode == "About the project":
+        st.write("""
+        This project is about recognizing hand gestures using OpenCV, TensorFlow, and MediaPipe.
+        The system captures video input, processes each frame to detect hand landmarks,
+        and then uses a trained model to predict the gesture being made.
+        """)
+    elif app_mode == "Run the project":
+        if st.button("Start"):
+            global p 
+            p = subprocess.Popen(["python", "Code.py"])
+            st.text("Running Hand Gesture Recognition script...")
+if __name__ == "__main__":
+    main()
